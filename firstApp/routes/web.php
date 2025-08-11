10c5b08c9b52488ea8837eab052526cac84c94eb
amnashahid7 <naheedamna5@gmail.com>
2025-08-11 16:30:25 +0500

    routes, views

--- firstApp/routes/web.php
+++ firstApp/routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
+});
+
+Route::get('/about',function(){
+    return view('about.index');
+});
+Route::get('/',function(){
+    return view('contact');
 });
