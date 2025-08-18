07c14a2f47f30344ca5253f44c119b089f9f48ba
amnashahid7 <naheedamna5@gmail.com>
2025-08-18 16:28:23 +0500

    curd start

---
+++ authentication/resources/views/products/create.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Create Product</h1>
+<h1>index</h1>
+<form action="{{route("product.store")}}" method="POST">
+@csrf
+
+    <div>
+        <label for="product_name">Product Name:</label>
+        <input type="text" id="product_name" name="product_name" required>
+    </div>
+    <div>
+        <label for="description">Description:</label>
+        <textarea id="description" name="description"></textarea>
+    </div>
+    <div>
+        <label for="price">Price:</label>
+        <input type="number" id="price" name="price" step="0.01" required>
+    </div>
+    <button type="submit">Create Product</button>
+
+</form>
+</body>
+</html>
